f3b9a9e0019016aa6ddbbe78644c62a2d024e2bc
Truong Thanh Loc <truongthanhloc@amitgroup.vn>
2022-04-18 08:34:19 +0700

    fix direc warmp

--- Model/components/Users.php
+++ Model/components/Users.php
@@ -1,31 +1,31 @@
 <?php
     function checkuser($user,$pass){
-        $sql="select * from customers where user='".$user."' and pass='".$pass."'";
+        $sql="select * from taikhoan where user='".$user."' and pass='".$pass."'";
         return getone($sql);
     }
 
     function adduser($user, $pass, $role, $name, $email){
-        $sql="INSERT INTO customers (user, pass, role, name, email) VALUES ('$user', '$pass', '$role', '$name', '$email')";
+        $sql="INSERT INTO taikhoan (user, pass, role, name, email) VALUES ('$user', '$pass', '$role', '$name', '$email')";
         return addsql($sql);
     }
 
     function updatepass($user,$pass){
-        $sql="UPDATE customers set pass='".$pass."' where user='".$user."'";
+        $sql="UPDATE taikhoan set pass='".$pass."' where user='".$user."'";
         return execsql($sql,0);
     }
     
     function finduser($user){
-        $sql="select * from customers where user='".$user."'";
+        $sql="select * from taikhoan where user='".$user."'";
         return getone($sql);
     }
 
     function getPassHash($user){
-        $sql="select pass from customers where user='".$user."'";
+        $sql="select pass from taikhoan where user='".$user."'";
         return getone($sql);
     }
 
     function get_user_by_id($user_id) {
-        $sql = "SELECT * FROM customers WHERE id = $user_id";
+        $sql = "SELECT * FROM taikhoan WHERE id = $user_id";
         return getone($sql);
     }
     
@@ -36,7 +36,7 @@
         $email = $update_info['email'];
         $name = $update_info['name'];
         $dob = $update_info['dob'];
-        $sql = "UPDATE customers SET address = '$address', phone = '$phone', name = '$name', date = '$dob', email = '$email' WHERE id = $user_id";
+        $sql = "UPDATE taikhoan SET address = '$address', phone = '$phone', name = '$name', date = '$dob', email = '$email' WHERE id = $user_id";
         execsql($sql, 1);
         return true;
     }
