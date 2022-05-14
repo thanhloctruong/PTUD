afed9fe07a72bcf8ddf3c1a477a5773afa5d4c60
hoanggchii <hoangthichi942001@gmail.com>
2022-05-14 20:48:30 +0700

    Hoang chi

--- Screen/HospitalWardEmployee/View/Home.php
+++ Screen/HospitalWardEmployee/View/Home.php
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<div>Đây là trang Home</div>
-<?php
-echo $_SESSION['id'];
-
-?>
-=======
-<div>Đây là trang Home</div>
->>>>>>> 554f3c96317afa1a9543070d35ebe6817aacb892
+<div>Đây là trang home</div>
